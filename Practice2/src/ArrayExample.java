d93b19b6766cf4e945e4aded74ca470e2aec44c7
nikithakolla30 <nikhithakolla30@gmail.com>
2023-03-07 18:14:58 -0800

    Array string Example 07-03-23

--- Practice2/src/ArrayExample.java
+++ Practice2/src/ArrayExample.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class ArrayExample {
+public class ArrayExample {
+    public static void main(String[] args) {
+        String[] arr = {"apple", "banana", "cherry", "date", "elderberry"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
