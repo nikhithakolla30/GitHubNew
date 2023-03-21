44db82bef56a3123f8adbea1377b86f594f21270
nikithakolla30 <nikhithakolla30@gmail.com>
2023-03-21 16:59:20 -0700

    Friday

---
+++ MapProject/src/MainClass1.java
@@ -0,0 +1,9 @@
+public class MainClass1 {
+    public static void main(String[] args){
+        Person person1= new Person();
+        person1.setName("Nikhitha");
+        person1.setAge(23);
+        System.out.println(person1.getName());
+        System.out.println(person1.getAge());
+    }
+}
